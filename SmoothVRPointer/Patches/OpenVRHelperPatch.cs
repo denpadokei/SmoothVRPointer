e351eecbee2898fe1e64bf06617ca7f1877c61e5
デンパ時計 <jsoldenpa@yahoo.co.jp>
2021-08-01 05:34:51 +0900

    プロジェクト ファイルを追加します。

---
+++ SmoothVRPointer/Patches/OpenVRHelperPatch.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using SmoothVRPointer.Configuration;
+using SmoothVRPointer.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace SmoothVRPointer.Patches
+{
+    [HarmonyPatch(
+        typeof(OpenVRHelper),
+        "AdjustControllerTransform",
+        new Type[] { typeof(XRNode), typeof(Transform), typeof(Vector3), typeof(Vector3) })]
+    public class OpenVRHelperPatch
+    {
+        public static void Prefix(ref XRNode node, ref Transform transform, ref Vector3 position, ref Vector3 rotation)
+        {
+            if (!PluginConfig.Instance.Enable) {
+                return;
+            }
+            if (CurrentSceneName == "GameCore") {
+                return;
+            }
+            PointerOffset.Offset(node, transform);
+        }
+        public static string CurrentSceneName { get; set; }
+    }
+}
---
+++ SmoothVRPointer/Utility/PointerOffset.cs
@@ -0,0 +1,50 @@
+﻿using SmoothVRPointer.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace SmoothVRPointer.Utility
+{
+    static class PointerOffset
+    {
+        public static void Offset(XRNode node, Transform transform)
+        {
+            Vector3 tmpPosition = Vector3.zero;
+            Quaternion tmpRotation = Quaternion.identity;
+            switch (node) {
+                case XRNode.LeftHand:
+                    if (beforeLeftTranceform == null) {
+                        var go = new GameObject();
+                        GameObject.DontDestroyOnLoad(go);
+                        beforeLeftTranceform = go.transform;
+                        return;
+                    }
+                    tmpPosition = Vector3.Lerp(beforeLeftTranceform.position, transform.position, Time.deltaTime * PluginConfig.Instance.OffsetValue);
+                    tmpRotation = Quaternion.Slerp(beforeLeftTranceform.rotation, transform.rotation, Time.deltaTime * PluginConfig.Instance.OffsetValue);
+                    transform.SetPositionAndRotation(tmpPosition, tmpRotation);
+                    beforeLeftTranceform.SetPositionAndRotation(transform.position, transform.rotation);
+                    break;
+                case XRNode.RightHand:
+                    if (beforeRightTranceform == null) {
+                        var go = new GameObject();
+                        GameObject.DontDestroyOnLoad(go);
+                        beforeRightTranceform = go.transform;
+                        return;
+                    }
+                    tmpPosition = Vector3.Lerp(beforeRightTranceform.position, transform.position, Time.deltaTime * PluginConfig.Instance.OffsetValue);
+                    tmpRotation = Quaternion.Slerp(beforeRightTranceform.rotation, transform.rotation, Time.deltaTime * PluginConfig.Instance.OffsetValue);
+                    transform.SetPositionAndRotation(tmpPosition, tmpRotation);
+                    beforeRightTranceform.SetPositionAndRotation(transform.position, transform.rotation);
+                    break;
+                default:
+                    return;
+            }
+        }
+        private static Transform beforeRightTranceform;
+        private static Transform beforeLeftTranceform;
+    }
+}
